f510471b96009a9db6a506ea1207a0b451007e51
Giresse_kifouani <mignongiresse@gmail.com>
2024-05-20 21:40:31 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'dm-distribution',
+  webDir: 'www'
+};
+
+export default config;
